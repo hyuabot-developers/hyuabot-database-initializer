5bd9d4fbbdd0f36b217bfa3ffb901543db5d27ae
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-17 21:07:47 +0900

    fix: Timestamptz 대신 timetz를 사용하도록 수정

--- utils/convert.py
+++ utils/convert.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 
 def convert_string_to_time(time: str) -> datetime:
-    return datetime.fromisoformat(f"1998-12-12T{time}:00+09:00")
+    return datetime.fromisoformat(f"{time}:00+09:00")
