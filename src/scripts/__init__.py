dcf5d89a21cc6b4d731b6fe96424171c6a53be1f
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-20 10:59:05 +0900

    fix: Typecheck 오류 수정

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import Session
 from scripts.bus import insert_bus_stop, insert_bus_route, insert_bus_route_stop
 from scripts.campus import insert_campus_data
 from scripts.restaurant import insert_restaurant_data
-from scripts.shuttle import insert_shuttle_period_type, insert_shuttle_period, insert_shuttle_route, \
-    insert_shuttle_stop, insert_shuttle_route_stop, insert_shuttle_timetable, \
+from scripts.shuttle import insert_shuttle_period_type, insert_shuttle_period, insert_shuttle_stop, \
     insert_commute_shuttle_route, insert_commute_shuttle_stop, insert_commute_shuttle_timetable
 from scripts.subway import insert_subway_route, insert_subway_station
 
@@ -12,10 +11,7 @@ from scripts.subway import insert_subway_route, insert_subway_station
 async def initialize_shuttle_data(db_session: Session):
     await insert_shuttle_period_type(db_session)
     await insert_shuttle_period(db_session)
-    await insert_shuttle_route(db_session)
     await insert_shuttle_stop(db_session)
-    await insert_shuttle_route_stop(db_session)
-    await insert_shuttle_timetable(db_session)
     await insert_commute_shuttle_route(db_session)
     await insert_commute_shuttle_stop(db_session)
     await insert_commute_shuttle_timetable(db_session)
